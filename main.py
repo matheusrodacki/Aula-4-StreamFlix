0b1e4d5cd0238444842f4040f4f6b50f4cec7a03
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-06-19 20:21:25 +0000

    implemented the __str methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Programa:
+
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
@@ -19,17 +20,30 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+
+
 class Filme(Programa):
+
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
+
 class Serie(Programa):
+
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 vingadores.dar_likes()
@@ -39,5 +53,7 @@ vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
 
-print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
-print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+listinha = [atlanta, vingadores]
+
+for programa in listinha:
+    print(programa)
