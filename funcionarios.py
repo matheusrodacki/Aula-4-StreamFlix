e9618319f8ff48c126f445b47de852444a8ddd7d
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2023-06-21 10:01:04 +0000

    implemented Hipster class

--- funcionarios.py
+++ funcionarios.py
@@ -1,5 +1,8 @@
 class Funcionario:
 
+    def __init__(self, nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print('Horas registradas.')
 
@@ -26,19 +29,27 @@ class Alura(Funcionario):
         print('Mostrando perguntas não respondidas do fórum')
 
 
+class Hipster:
+
+    def __str__(self):
+        return f'Hipster,  {self.nome}'
+
+
 class Junior(Alura):
     pass
 
 
-class Pleno(Alura, Caelum):
+class Pleno(Alura, Caelum, Hipster):
     pass
 
 
-jose = Junior()
+jose = Junior('José')
 jose.busca_perguntas_sem_resposta()
 
-luan = Pleno()
+luan = Pleno('Luan')
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 
 luan.mostrar_tarefas()
+
+print(luan)
